86e1db62d422b6b9178ffc3541b9ba94fa0c5ef7
aramrattan <arlina.ramrattan@gmail.com>
2017-12-28 14:23:09 -0500

    added UserSchema

---
+++ apitanic/model/user.py
@@ -0,0 +1,8 @@
+from marshmallow import Schema, fields
+
+class UserSchema(Schema):
+    name = fields.Str()
+    email = fields.Email()
+    friends = fields.List(fields.Str())
+    favourites = fields.List(fields.Str())
+    
