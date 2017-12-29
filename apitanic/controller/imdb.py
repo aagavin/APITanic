47764444bde1ccf8513124c5bc00f286eee20fa6
aagavin <aagavin@gmail.com>
2017-12-29 14:19:23 -0500

    imdb openapi

--- apitanic/controller/imdb.py
+++ apitanic/controller/imdb.py
@@ -4,19 +4,26 @@ from imdbpie import Imdb
 from sanic.response import json
 from sanic.request import Request
 from sanic import Blueprint
+from sanic_openapi import doc
 
 imdb = Imdb()
 
 imdbBlueprint = Blueprint('imdb', url_prefix='imdb')
 
 
-@imdbBlueprint.route('/title/<imdbid>', methods=['GET'])
+@imdbBlueprint.route('/title/<imdbid>/', methods=['GET'])
+@doc.summary("get movie by imdbid")
+@doc.consumes({"imdbid": str}, location="path")
+@doc.produces({'data': {'movie': object}})
 async def title_by_id(request: Request, imdbid: str):
     title = imdb.get_title_by_id(imdbid)
-    return json({'data': title})
+    return json({'data': {'movie': title}})
 
 
-@imdbBlueprint.route('/search', methods=['GET'])
+@imdbBlueprint.route('/search/', methods=['GET'])
+@doc.summary("Search movies in imdb")
+@doc.consumes({"q": str}, location="query")
+@doc.produces({'data': list})
 async def search_movie(request: Request):
     movie_query = request.args['q'][0]
     url = [f"https://v2.sg.media-imdb.com/suggests/{movie_query[0]}/{movie_query}.json"]
@@ -27,6 +34,8 @@ async def search_movie(request: Request):
     return json({'data': movies['d']})
 
 
-@imdbBlueprint.route('/popular', methods=['GET'])
+@imdbBlueprint.route('/popular/', methods=['GET'])
+@doc.summary("Most popular movies")
+@doc.produces({'data': {'movies': object}})
 async def popular_movies(request: Request):
     return json({'data': {'movies': imdb.popular_movies()}})
