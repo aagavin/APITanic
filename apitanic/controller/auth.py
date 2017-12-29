b3bfcf0d11f36f31a77a8d2395bacbe2e91ff7d1
aagavin <aagavin@gmail.com>
2017-12-29 14:14:57 -0500

    more openapi

--- apitanic/controller/auth.py
+++ apitanic/controller/auth.py
@@ -2,13 +2,17 @@ from apitanic.model.firebase import Firebase
 from sanic.response import json
 from sanic.request import Request
 from sanic import Blueprint
+from sanic_openapi import doc
 
 
 userBlueprint = Blueprint('user', url_prefix='user')
 firebase = Firebase()
 
 
-@userBlueprint.route('/create', methods=['POST'])
+@userBlueprint.route('/create/', methods=['POST'])
+@doc.summary("Create a new account")
+@doc.consumes({"user": str, "displayName": str, "email": str}, location="body")
+@doc.produces({'data': {'token': str}})
 async def create_accoutn(request: Request):
     token = firebase.create_account(
         request.json['email'],
